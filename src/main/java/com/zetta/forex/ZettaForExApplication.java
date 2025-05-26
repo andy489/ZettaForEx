111e71d7e779bbb213bb3bba7662edc0811cfd4c
andy489 <stoev.andy@gmail.com>
2025-05-26 14:22:38 +0300

    Initial Commit

---
+++ src/main/java/com/zetta/forex/ZettaForExApplication.java
@@ -0,0 +1,13 @@
+package com.zetta.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZettaForExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZettaForExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zetta/forex/ZettaForExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetta.forex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZettaForExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
